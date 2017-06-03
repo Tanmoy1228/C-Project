d923702d3e66de04f8546018c3439bc1d0b77fd7
Tanmoy Ghosh <tanmoy1228@gmail.com>
2017-06-03 07:42:48 +0600

    Final
    Correction in Show_Connections and Unfriend function

--- main.cpp
+++ main.cpp
@@ -117,7 +117,7 @@ Search_System::Search_System(Database *Base)
     Main_Base = Base;
 }
 
-int Search_Subfriends(Friend *Friend_Obj, vector<string> *Checked, vector<string> *Track_Strings,string Start_Search, string Search_For)
+void Search_Subfriends(Friend *Friend_Obj, vector<string> *Checked, vector<string> *Track_Strings,string Start_Search, string Search_For)
 {
     int I;
     int II;
@@ -131,21 +131,11 @@ int Search_Subfriends(Friend *Friend_Obj, vector<string> *Checked, vector<string
         Current_Friend = Friend_Obj->Person_Friends[I];
         string temp = Friend_Obj->Tag + ">" + Current_Friend->Person_Name;
 
-        Current_Friend->Tag = temp;
-
         for (II = 0; II < Checked->size(); II++)
         {
             if (Current_Friend->Person_Name == (*Checked)[II])
             {
-                Pass = true;
-                if (Start_Search != Current_Friend->Person_Name)
-                {
-                    Current_Friend->Tag = "";
-                }
-                else
-                {
-                    Current_Friend->Tag = Start_Search;
-                }
+                Pass=true;
                 break;
             }
         }
@@ -153,6 +143,7 @@ int Search_Subfriends(Friend *Friend_Obj, vector<string> *Checked, vector<string
 
         if (Pass == false)
         {
+            Current_Friend->Tag = temp;
             if (Current_Friend->Person_Name == Search_For)
             {
                 //We found him
@@ -162,9 +153,8 @@ int Search_Subfriends(Friend *Friend_Obj, vector<string> *Checked, vector<string
             else
             {
                 Checked->push_back(Current_Friend->Person_Name);
-
                 Search_Subfriends(Current_Friend, Checked, Track_Strings,Start_Search, Search_For);
-
+                Checked->erase(Checked->end());
             }
         }
         else
@@ -172,12 +162,24 @@ int Search_Subfriends(Friend *Friend_Obj, vector<string> *Checked, vector<string
             Pass = false;
         }
     }
-    return -1;
+    return;
 }
 
 void Search_System::Show_Connections(string Name1, string Name2)
 {
-    if (Main_Base->Contain(Name1) != -1)
+    if (Main_Base->Contain(Name1) == -1 && Main_Base->Contain(Name2) == -1)
+    {
+        cout <<Name1<<" and "<<Name2<< " not found." << endl;
+    }
+    else if(Main_Base->Contain(Name1) == -1)
+    {
+        cout <<Name1<< " not found." << endl;
+    }
+    else if(Main_Base->Contain(Name2)==-1)
+    {
+        cout <<Name2<< " not found." << endl;
+    }
+    else
     {
         Friend *Friend_Obj = &Main_Base->All_People[Main_Base->Contain(Name1)];
         vector <string> Checked_Names;
@@ -207,14 +209,10 @@ void Search_System::Show_Connections(string Name1, string Name2)
                     cout << endl;
             }
         }
-    }
-    else
-    {
-        cout << "Name not found." << endl;
+        else
+            cout<<"No Connection Found Between "<<Name1<<" and "<<Name2<<"."<<endl;
     }
 }
-
-
 
 void All_Database_Functions(Database &Base)
 {
@@ -224,7 +222,7 @@ void All_Database_Functions(Database &Base)
         cout << endl;
         cout << "What do you want to do ?" << endl;
         cout << "1. Add people" << endl;
-        cout << "2. Remove people" << endl;
+        cout << "2. Unfriend Someone" << endl;
         cout << "3. Full list" << endl;
         cout << "4. Make friendship" << endl;
         cout << "5. Someone's friendlist" << endl;
@@ -256,7 +254,7 @@ void All_Database_Functions(Database &Base)
             for (I = 0; I < Base.All_People.size(); I++)
                 cout << I+1 << ". " << Base.All_People[I].Person_Name << endl;
             cout << "-------------------------------" << endl;
-            cout << "Enter ID to remove (0 to back) : " << endl;
+            cout << "Enter ID to select (0 to back) : " << endl;
 
             cin >> ID;
             if (ID == 0)
@@ -265,9 +263,61 @@ void All_Database_Functions(Database &Base)
             }
             else if (ID > 0 && ID <= Base.All_People.size())
             {
-                //Base.All_People[ID-1].Unfriend_All();
-                Base.All_People.erase(Base.All_People.begin()+ID-1);
-                cout << "ID : " << ID << " removed successfully." << endl;
+Try_Again_1:
+                cout << endl;
+                cout << Base.All_People[ID-1].Person_Name << " is selected." << endl;
+                cout << "Person has : " << Base.All_People[ID-1].Person_Friends.size() << " friends" << endl;
+                cout << "-------------------------------" << endl;
+                bool Friend_With;
+                for (I = 0; I < Base.All_People.size(); I++)
+                {
+                    if (I != ID-1)
+                    {
+                        Friend_With = Base.All_People[ID-1].Is_Friend_With(&Base.All_People[I]);
+                        if (Friend_With == false)
+                            cout << I+1 << ". " <<  Base.All_People[I].Person_Name << endl;
+                        else
+                            cout << I+1 << ". " <<  Base.All_People[I].Person_Name << " < friend." << endl;
+                    }
+                    else
+                    {
+                        cout << I+1 << ". " <<  Base.All_People[I].Person_Name << " < selected." << endl;
+                    }
+                }
+                cout << "-------------------------------" << endl;
+                cout << "Enter ID to remove from friendlist (0 to back) : " << endl;
+                int ID_2;
+                cin >> ID_2;
+                if (ID_2 == 0)
+                {
+
+                }
+                else if (ID_2 > 0 && ID_2 <= Base.All_People.size())
+                {
+                    int Sel_ID = ID_2 - 1;
+                    if (Sel_ID == ID - 1)
+                    {
+                        //Selected person
+                        cout << "Selected person cannot be removed from his/her friendlist!. Try again." << endl;
+                        goto Try_Again_1;
+                    }
+                    Friend_With = Base.All_People[ID-1].Is_Friend_With(&Base.All_People[Sel_ID]);
+                    if (Friend_With == false)
+                    {
+                        //Already friend
+                        cout << "Selected person is not friend with " << Base.All_People[Sel_ID].Person_Name << ". Try again." << endl;
+                        goto Try_Again_1;
+                    }
+                    Base.All_People[Sel_ID].Unfriend(&Base.All_People[ID-1]);
+                    Base.All_People[ID-1].Unfriend(&Base.All_People[Sel_ID]);
+                    cout << Base.All_People[Sel_ID].Person_Name << " is removed from " << Base.All_People[ID-1].Person_Name <<"'s friendlist."<< endl;
+                    break;
+                }
+                else
+                {
+                    cout << "Invalid ID. Try again." << endl;
+                    goto Try_Again_1;
+                }
             }
             else
             {
@@ -336,19 +386,18 @@ Try_Again_2:
                     if (Friend_With == true)
                     {
                         //Already friend
-                        cout << "Selected person is already friend with " << Sel_ID+1 << ". Try again." << endl;
+                        cout << "Selected person is already friend with " << Base.All_People[Sel_ID].Person_Name << ". Try again." << endl;
                         goto Try_Again_2;
                     }
                     Base.All_People[ID-1].Make_Friendship(&Base.All_People[Sel_ID]);
-                    break;
                     cout << Base.All_People[ID-1].Person_Name << " is now friend with " << Base.All_People[Sel_ID].Person_Name << endl;
+                    break;
                 }
                 else
                 {
                     cout << "Invalid ID. Try again." << endl;
                     goto Try_Again_2;
                 }
-
             }
             else
             {
@@ -372,6 +421,8 @@ Try_Again_2:
             {
                 for (I = 0; I <  Base.All_People[ID-1].Person_Friends.size(); I++)
                     cout << I+1 << ". " << Base.All_People[ID-1].Person_Friends[I]->Person_Name<< endl;
+                if(Base.All_People[ID-1].Person_Friends.size()==0)
+                    cout<< Base.All_People[ID-1].Person_Name<<" has no friend."<<endl;
                 cout << "-------------------------------" << endl;
             }
             else
@@ -412,6 +463,7 @@ int main()
     int Rima = Base.Add_People("Rima");
     int Muna = Base.Add_People("Muna");
     int Abir = Base.Add_People("Abir");
+    int Soma = Base.Add_People("Soma");
 
     Base.All_People[Amit].Make_Friendship(&Base.All_People[Fahad]);
     Base.All_People[Fahad].Make_Friendship(&Base.All_People[Tanmoy]);
@@ -423,31 +475,37 @@ int main()
     Base.All_People[Abir].Make_Friendship(&Base.All_People[Rima]);
     Base.All_People[Abir].Make_Friendship(&Base.All_People[Amit]);
 
+//    Base.All_People[Amit].Unfriend(&Base.All_People[Fahad]);
+//    Base.All_People[Fahad].Unfriend(&Base.All_People[Amit]);
+
     int Continue = 1;
     while (Continue)
     {
         cout << endl << endl;
-        cout << "1. Database" << endl;
-        cout << "2. Search" << endl;
+        cout << "1. Check or Edit Friendlist" << endl;
+        cout << "2. Check Friendship between Two People" << endl;
         cout << "3. Exit" << endl;
 
         int Choice;
         cin >> Choice;
 
-        switch (Choice)
+        if(Choice==1)
         {
-        case 1:
             All_Database_Functions(Base);
-            break;
-        case 2:
+        }
+        else if(Choice==2)
+        {
             All_Search_Functions(Search);
+        }
+        else if(Choice==3)
+        {
             break;
-        case 3:
-            Continue = 0;
-            break;
-        default:
+        }
+        else
+        {
             cout << "Invalid choice. Try again." << endl;
         }
     }
+
     return 0;
 }
